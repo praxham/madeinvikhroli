f7345fd733c58496fa4300c6f76280e224cd5daf
pratham patankar <prathampatankar1234@gmail.com>
2024-10-20 23:24:24 +0530

    api call fixed

--- src/API/getArtifacts.js
+++ src/API/getArtifacts.js
@@ -1,6 +1,5 @@
 import { getData } from "./APICalls"
 
 export const getArtifacts = () => {
-    const response = getData("/api/v1/artifacts")
-    return response
+    return getData("/api/v1/artifacts")
 }
