099d069bb51e5905e059c2da0cf6749703d1c4ad
pratham patankar <prathampatankar1234@gmail.com>
2024-10-22 20:55:15 +0530

    backend url fixed 2

--- src/API/APICalls.js
+++ src/API/APICalls.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const commonPoint = import.meta.env.VITE_VERCEL_BACKEND_URL;
+export const commonPoint = import.meta.env.VITE_VERCEL_BACKEND_URL || process.env.VITE_VERCEL_BACKEND_URL;
 
 export const postData = async (endPoint = "", data, type = false) => {
   try {
